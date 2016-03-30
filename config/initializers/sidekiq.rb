95e8d228ac687b02cbdf9bfa49c4f6d982e2ac86
dtaniwaki <daisuketaniwaki@gmail.com>
2016-03-29 17:55:07 -0700

    Add more redis options

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Settings.redis.sidekiq.url, namespace: Settings.redis.sidekiq.namespace }
+  config.redis = Settings.redis.sidekiq_server.to_hash
   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Settings.redis.sidekiq.url, namespace: Settings.redis.sidekiq.namespace }
+  config.redis = Settings.redis.sidekiq_client.to_hash
 end
 
 Sidekiq.default_worker_options = { backtrace: true }
