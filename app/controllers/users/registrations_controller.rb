9a15aec66319b2f89fb2a46a595fd2f4b76a510c
dtaniwaki <daisuketaniwaki@gmail.com>
2016-03-31 01:42:31 -0700

    Permit User's name change

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,5 +2,17 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     skip_after_action :verify_authorized
     skip_after_action :verify_policy_scoped
+    before_action :configure_permitted_parameters
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:name, :email, :password, :password_confirmation)
+      end
+      devise_parameter_sanitizer.for(:account_update) do |u|
+        u.permit(:name, :email, :password, :password_confirmation, :current_password)
+      end
+    end
   end
 end
