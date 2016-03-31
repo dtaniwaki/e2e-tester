cf9f36d57c21cb457c42477b50d4244c0d11e870
dtaniwaki <daisuketaniwaki@gmail.com>
2016-03-31 12:22:24 -0700

    Fix coverage start point

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
-require 'pundit/rspec'
-require 'fakeredis/rspec'
 
 # Test Coverage
 require 'codeclimate-test-reporter'
@@ -17,6 +12,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   CodeClimate::TestReporter::Formatter
 ]
 SimpleCov.start 'rails'
+
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'pundit/rspec'
+require 'fakeredis/rspec'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
