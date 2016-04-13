9ef83ebb874d26bd331374ba2ae65d1a7eb8486b
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-13 15:16:16 -0700

    Add the spec for auto assign the onwer's user_project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:project) { create :project }
+  describe 'after_create :assign_owner!' do
+    subject(:project) { build :project }
+    it 'assigns user_project for the owner' do
+      expect(subject.user_projects.map(&:user)).to eq []
+      subject.save!
+      expect(subject.user_projects.map(&:user)).to eq [subject.user]
+    end
+  end
 end
