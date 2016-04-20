baaf31ba0ed6de6b6d8b34eee96562cc6b99215b
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-19 17:17:43 -0700

    Create user shared test step set relation on shared test step set create

--- app/models/shared_test_step_set.rb
+++ app/models/shared_test_step_set.rb
@@ -1,3 +1,11 @@
 class SharedTestStepSet < TestStepSet
   has_many :user_shared_test_step_sets, inverse_of: :shared_test_step_set
+
+  after_create :assign_owner!
+
+  private
+
+  def assign_owner!
+    user_shared_test_step_sets.create!(user: user)
+  end
 end
