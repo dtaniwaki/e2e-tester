2f91f732e9f2acf92bf65ccc0004b055892ef4c3
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-20 21:09:31 -0700

    Add useful scopes in UserTestStepSetShare model

--- app/models/user_shared_test_step_set.rb
+++ app/models/user_shared_test_step_set.rb
@@ -1,4 +1,7 @@
 class UserSharedTestStepSet < ApplicationRecord
   belongs_to :user, inverse_of: :user_shared_test_step_sets
   belongs_to :shared_test_step_set, inverse_of: :user_shared_test_step_sets
+
+  scope :with_user, ->(user) { where(user_id: user.is_a?(ActiveRecord::Base) ? user.id : user) }
+  scope :with_share_test_step_set, ->(stss) { where(shared_test_step_set_id: stss.is_a?(ActiveRecord::Base) ? stss.id : stss) }
 end
