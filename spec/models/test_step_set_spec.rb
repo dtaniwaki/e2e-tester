b6990124d39bd6efee4a1160ee863c466e57c098
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-21 21:00:48 -0700

    Organize same_test_step_set? spec

--- spec/models/test_step_set_spec.rb
+++ spec/models/test_step_set_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 RSpec.describe TestStepSet, type: :model do
   subject { create :test }
+  describe '#same_test_step_set?' do
+    let(:browsers) { create_list(:browser, 1) }
+    let(:base_test_step_set) { create :test }
+    subject { create :test, base_test_step_set: base_test_step_set, title: 'foo', description: 'foo', browsers: browsers }
+    it 'returns true for a cloned instance' do
+      expect(subject.same_test_step_set?(subject.clone)).to be true
+    end
+    context 'against the test step set' do
+      let(:target) { create :shared_test_step_set, base_test_step_set: base_test_step_set, title: 'foo', description: 'foo' }
+      it 'returns false if the other instance is a shared test step set' do
+        expect(subject.same_test_step_set?(target)).to be false
+      end
+    end
+    context 'for other instance' do
+      let(:target) { create :test, base_test_step_set: base_test_step_set, title: 'foo', description: 'foo', browsers: browsers }
+      before do
+        expect(subject).not_to eq target
+      end
+      it 'returns true for the same contents' do
+        expect(subject.same_test_step_set?(target)).to be true
+      end
+      context 'test_step_set_id is different' do
+        let(:target) { create :test, base_test_step_set: create(:test), title: 'foo', description: 'foo', browsers: browsers }
+        it 'returns false' do
+          expect(subject.same_test_step_set?(target)).to be false
+        end
+      end
+      context 'test_steps is different' do
+        it 'returns false' do
+          target.assign_attributes(test_steps_attributes: subject.test_steps_attributes + "\nNone")
+          expect(subject.same_test_step_set?(target)).to be false
+        end
+      end
+    end
+  end
   describe 'validate :validate_same_test_step_set' do
     let(:base_test_step_set) { create :test }
     context 'against the base test with the same content' do
