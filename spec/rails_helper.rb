525d2f4cf71e7dfc36d10caa04323ffbb8f801cf
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-24 00:56:37 -0700

    Set sidekiq test mode

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'pundit/rspec'
 require 'fakeredis/rspec'
+require 'sidekiq/testing'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
