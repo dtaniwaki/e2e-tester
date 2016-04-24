9655b5266bdc84d1fa73cba16e771bb202ba2283
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-23 21:36:25 -0700

    Add DeviseHelper spec

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,9 +3,9 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-error alert-danger"> <button type="button"
-    class="close" data-dismiss="alert">×</button>
+    html = <<-HTML.strip_heredoc
+    <div class="alert alert-error alert-danger">
+      <button type="button" class="close" data-dismiss="alert">×</button>
       #{messages}
     </div>
     HTML
--- spec/helpers/devise_helper_spec.rb
+++ spec/helpers/devise_helper_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe DeviseHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#devise_error_messages!' do
+    let(:resource) { create :user }
+    before do
+      local_resource = resource
+      helper.define_singleton_method :resource do
+        local_resource
+      end
+    end
+    it 'returns error messages' do
+      expect(helper.devise_error_messages!).to eq ''
+    end
+    context 'with errors' do
+      let(:message) { %(<div class="alert alert-error alert-danger">\n  <button type="button" class="close" data-dismiss="alert">×</button>\n  <li>foo</li>\n</div>\n) }
+      it 'returns error messages' do
+        resource.errors.add :base, 'foo'
+        expect(helper.devise_error_messages!).to eq message
+      end
+    end
+  end
 end
