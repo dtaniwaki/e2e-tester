0ecf1a7377457e3e14ae16147e9bc9831ec47236
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-26 12:07:53 -0700

    Fix the output tag structure of bootstrap status message helper

--- app/helpers/bootstrap_helper.rb
+++ app/helpers/bootstrap_helper.rb
@@ -2,11 +2,11 @@ module BootstrapHelper
   def bootstrap_status_tag(tag_name, message, ok, ng = !ok)
     if ok
       content_tag tag_name, class: 'text-success' do
-        content_tag :span, message, class: 'glyphicon glyphicon-ok'
+        content_tag(:span, '', class: 'glyphicon glyphicon-ok') + message
       end
     elsif ng
       content_tag tag_name, class: 'text-danger' do
-        content_tag :span, message, class: 'glyphicon glyphicon-remove'
+        content_tag(:span, '', class: 'glyphicon glyphicon-remove') + message
       end
     else
       content_tag tag_name, message
--- spec/helpers/bootstrap_helper_spec.rb
+++ spec/helpers/bootstrap_helper_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe BootstrapHelper, type: :helper do
   describe '#bootstrap_status_tag' do
     context 'with ok status' do
       it 'returns message with status icon' do
-        expect(helper.bootstrap_status_tag(:span, 'foo', true, false)).to eq '<span class="text-success"><span class="glyphicon glyphicon-ok">foo</span></span>'
+        expect(helper.bootstrap_status_tag(:span, 'foo', true, false)).to eq '<span class="text-success"><span class="glyphicon glyphicon-ok"></span>foo</span>'
       end
     end
     context 'with ng status' do
       it 'returns message with status icon' do
-        expect(helper.bootstrap_status_tag(:span, 'foo', false, true)).to eq '<span class="text-danger"><span class="glyphicon glyphicon-remove">foo</span></span>'
+        expect(helper.bootstrap_status_tag(:span, 'foo', false, true)).to eq '<span class="text-danger"><span class="glyphicon glyphicon-remove"></span>foo</span>'
       end
     end
     context 'with ok and ng status' do
       it 'returns message with status icon' do
-        expect(helper.bootstrap_status_tag(:span, 'foo', true, true)).to eq '<span class="text-success"><span class="glyphicon glyphicon-ok">foo</span></span>'
+        expect(helper.bootstrap_status_tag(:span, 'foo', true, true)).to eq '<span class="text-success"><span class="glyphicon glyphicon-ok"></span>foo</span>'
       end
     end
     context 'with non ok and non ng status' do
@@ -25,12 +25,12 @@ RSpec.describe BootstrapHelper, type: :helper do
     context 'default ng' do
       context 'with ok status' do
         it 'returns message with status icon' do
-          expect(helper.bootstrap_status_tag(:span, 'foo', true)).to eq '<span class="text-success"><span class="glyphicon glyphicon-ok">foo</span></span>'
+          expect(helper.bootstrap_status_tag(:span, 'foo', true)).to eq '<span class="text-success"><span class="glyphicon glyphicon-ok"></span>foo</span>'
         end
       end
       context 'with non ok status' do
         it 'returns message with status icon' do
-          expect(helper.bootstrap_status_tag(:span, 'foo', false)).to eq '<span class="text-danger"><span class="glyphicon glyphicon-remove">foo</span></span>'
+          expect(helper.bootstrap_status_tag(:span, 'foo', false)).to eq '<span class="text-danger"><span class="glyphicon glyphicon-remove"></span>foo</span>'
         end
       end
     end
