3d11c6376def66d85353d133030958bef6bf6376
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-27 11:48:35 -0700

    Fix the rubocop error

--- spec/misc/seed_spec.rb
+++ spec/misc/seed_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'seed.rb' do
+RSpec.describe 'seed.rb' do # rubocop:disable RSpec/DescribeClass
   it 'succeeds' do
     expect(User.count).to be 0
     expect do
