08c7c72ba91b8f6ffae9fb0ef1744e9ab7d1751a
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-07 16:26:19 -0700

    Refine Test in admin

--- app/admin/test.rb
+++ app/admin/test.rb
@@ -5,9 +5,17 @@ ActiveAdmin.register Test do
   scope :with_deleted
   scope :only_deleted
 
+  controller do
+    def scoped_collection
+      super.includes :user, :current_test_version
+    end
+  end
+
   index do
     selectable_column
     id_column
+    column(:user_id) { |t| link_to t.user.name, admin_user_path(t.user) }
+    column(:curret_test_version_id) { |t| link_to t.current_test_version.id, admin_test_version_path(t.current_test_version) if t.current_test_version.present? }
     column :title
     column :created_at
     column :updated_at
@@ -17,22 +25,33 @@ ActiveAdmin.register Test do
   show do |test|
     attributes_table do
       test.attribute_names.each do |name|
-        if name == 'test_version_id'
-          row name do |p|
-            link_to p.test_version_id, admin_test_path(p.test_version_id) if p.test_version_id.present?
+        case name
+        when 'user_id'
+          row name do |t|
+            link_to t.user.name, admin_user_path(t.user)
+          end
+        when 'current_test_version_id'
+          row name do |t|
+            link_to t.current_test_version.id, admin_test_version_path(t.current_test_version) if t.current_test_version.present?
           end
         else
           row name
         end
       end
     end
-    panel 'Tests' do
+    panel 'Test Versions' do
       table_for test.test_versions do
         column :id do |t|
-          link_to t.id, admin_test_path(t)
+          link_to t.id, admin_test_version_path(t)
+        end
+        column :position do |t|
+          link_to t.position, admin_test_version_path(t)
+        end
+        column :current do |t|
+          test.current_test_version == t ? 'Y' : ''
         end
         column :created_at do |t|
-          link_to t.created_at, admin_test_path(t)
+          link_to t.created_at, admin_test_version_path(t)
         end
       end
     end
