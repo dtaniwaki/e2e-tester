46c51b45766e75ce504ddf8bfdc942a4ab655329
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-07 21:05:26 -0700

    Destroy variables dependently

--- app/models/user_test.rb
+++ app/models/user_test.rb
@@ -2,7 +2,7 @@ class UserTest < ApplicationRecord
   belongs_to :user, inverse_of: :accessible_tests
   belongs_to :assigned_by, class_name: 'User', foreign_key: 'assigned_by_id', inverse_of: :assigned_test_users
   belongs_to :test, inverse_of: :user_tests
-  has_many :user_test_variables, inverse_of: :user_test
+  has_many :user_test_variables, inverse_of: :user_test, dependent: :destroy
 
   scope :with_user, ->(user) { where(user_id: user.is_a?(ActiveRecord::Base) ? user.id : user) }
   scope :with_test, ->(test) { where(test_id: test.is_a?(ActiveRecord::Base) ? test.id : test) }
