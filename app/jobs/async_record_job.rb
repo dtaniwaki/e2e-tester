7b1f087e39c1d7cc5c1fd1bf24a484d3209acc9c
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-08 01:42:51 -0700

    Set default queue for AsyncRecordJob

--- app/jobs/async_record_job.rb
+++ app/jobs/async_record_job.rb
@@ -1,7 +1,7 @@
 class AsyncRecordJob < ApplicationJob
   include Sidekiq::Worker
 
-  sidekiq_options queue: :async_record, retry: true, backtrace: true, unique: :while_executing
+  sidekiq_options queue: :default, retry: true, backtrace: true, unique: :while_executing
 
   def perform(klass, id, method_name)
     record = klass.constantize.find_by_id!(id)
