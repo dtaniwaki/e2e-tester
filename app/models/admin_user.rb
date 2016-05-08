1d4af886c76c5401200ce6b449ab2d427784124a
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-07 20:30:30 -0700

    Add logical delete on AdminUser

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,6 +3,8 @@ class AdminUser < ApplicationRecord
 
   has_many :comments, as: :commenter, inverse_of: :commenter
 
+  acts_as_paranoid
+
   validates :name, :email, :encrypted_password, length: { maximum: 255 }, presence: true
   validates :email, :confirmation_token, :reset_password_token, :unlock_token, uniqueness: true, allow_blank: true
 end
