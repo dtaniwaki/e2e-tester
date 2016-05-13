a8299d44feb8ded36ba80b8727a73db3c5b602e8
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-13 02:32:41 -0700

    Set locale by request

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,8 +2,10 @@ class BaseController < ApplicationController
   include Pundit
   include PunditExt
   include OriginConcern
+  include LocaleConcern
   include UrlHelper
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
   before_action :authenticate_user!
+  before_action :set_locale
 end
