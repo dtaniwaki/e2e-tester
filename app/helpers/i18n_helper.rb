fc6b275705c639369b092467742f83c7d1cfb69a
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-15 13:29:55 -0700

    Add I18nHelper

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -1,8 +1,13 @@
 module I18nHelper
-  def translate(key, options={})
-    super(key, options.reverse_merge(raise: true))
-  rescue I18n::MissingTranslationData
-    key
+  def translate(key, options = {})
+    options = options.dup
+    options[:raise] = true
+    if key.to_s !~ /^(devise|simple_form|activerecord|models)/
+      options[:scope] = ['views']
+    end
+    super(key, options)
+  rescue I18n::MissingTranslationData => e
+    options.key?(:default) ? options[:default] : e.keys.join('.')
   end
-  alias :t :translate
+  alias t translate
 end
