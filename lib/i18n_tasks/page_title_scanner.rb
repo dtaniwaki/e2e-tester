4e88f030f1a01688204180dc2d861be802649745
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-16 17:56:11 -0700

    Define PageTitleScanner only if i18n-tasks is available

--- lib/i18n_tasks/page_title_scanner.rb
+++ lib/i18n_tasks/page_title_scanner.rb
@@ -1,22 +1,24 @@
-module I18nTasks
-  class PageTitleScanner < ::I18n::Tasks::Scanners::FileScanner
-    include I18n::Tasks::Scanners::OccurrenceFromPosition
+if defined?(I18n::Tasks)
+  module I18nTasks
+    class PageTitleScanner < ::I18n::Tasks::Scanners::FileScanner
+      include I18n::Tasks::Scanners::OccurrenceFromPosition
 
-    protected
+      protected
 
-    def scan_file(path)
-      return [] unless path =~ %r{(views)/(.+/[^_/][^./]+)[^/]+$}
-      relative_path = "#{Regexp.last_match(1)}/#{Regexp.last_match(2)}"
-      return [] if Regexp.last_match(2).start_with?('layouts')
-      return [] if Regexp.last_match(2) =~ /[_]?mailer\b/
-      results = []
-      File.open(path) do |f|
-        text = f.read
-        unless text =~ /t\(['"]\.title['"]\)/
-          results << ["#{relative_path.tr('/', '.')}.title", occurrence_from_position(path, text, 0)]
+      def scan_file(path)
+        return [] unless path =~ %r{(views)/(.+/[^_/][^./]+)[^/]+$}
+        relative_path = "#{Regexp.last_match(1)}/#{Regexp.last_match(2)}"
+        return [] if Regexp.last_match(2).start_with?('layouts')
+        return [] if Regexp.last_match(2) =~ /[_]?mailer\b/
+        results = []
+        File.open(path) do |f|
+          text = f.read
+          unless text =~ /t\(['"]\.title['"]\)/
+            results << ["#{relative_path.tr('/', '.')}.title", occurrence_from_position(path, text, 0)]
+          end
         end
+        results
       end
-      results
     end
   end
 end
