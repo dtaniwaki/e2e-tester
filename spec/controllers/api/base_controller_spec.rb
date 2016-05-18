68bb2901caf23c2c42fc41c606e91a86a60e6812
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-18 14:50:16 -0700

    Remove debug code

--- spec/controllers/api/base_controller_spec.rb
+++ spec/controllers/api/base_controller_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 RSpec.describe Api::BaseController, type: :controller do
   controller do
     def index
-      raise RuntimeError, 'foo'
+      raise 'foo'
     end
   end
 
   describe 'rescue_from' do
     it 'returns 500 and the message in json' do
-      puts controller.inspect
       get :index
 
       expect(response.status).to be 500
