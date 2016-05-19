c0ceed50e3398886d76200b900163a84914e6fa0
dtaniwaki <daisuketaniwaki@gmail.com>
2016-05-18 23:58:32 -0700

    Disallow cors credentials

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
     allow do
       origins '*'
-      resource '/api/*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+      resource '/api/*', headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head], credentials: false
     end
   end
 end
